2fdd664d03cd51566506e0e9358d49d984b9b6d7
borhan uddin <borhan uddin>
2022-05-21 23:21:12 +0600

    make it nojhdddfdgdfw

--- pages/index.js
+++ pages/index.js
@@ -290,7 +290,6 @@ const Blogs = ({ sports, technology, education, health, lifestyle, artlit, relig
             <div className='col-md-3'>
               <div className={styles.specialsec}>
                 {/* <img src='img1.jpg' className='w-100 h-100' /> */}
-                {/* <img src='img1.jpg' className='w-100 h-100' /> */}
                 
                 <img src={`${API}/blog/photo/${special[0]?.slug}`} className='w-100 h-100'/>
                 <h4 className={styles.bottomleft}>
