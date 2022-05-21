7889074a3ea23939c20082750bbfd6aa86f0afda
borhan uddin <borhan uddin>
2022-05-21 22:51:56 +0600

    make it nojhw

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ const {publicRuntimeConfig} = getConfig()
 
 export const API = 'https://chnews-api.herokuapp.com/api';
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
-export const DOMAIN = "chalnews.herokuapp.com";
-
+export const DOMAIN = "fr-ne.herokuapp.com";
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 // publicRuntimeConfig.PRODUCTION 
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   publicRuntimeConfig:{
     
     APP_NAME: 'NewYork News',
-    API_PRODUCTION: 'https://chalaman2.herokuapp.com/api',
+    API_PRODUCTION: 'https://chnews-api.herokuapp.com/api',
     API_DEVELOPMENT: 'http://localhost:8000/api',
     PRODUCTION: true,
     FB_APP_ID: "facebook.com"
